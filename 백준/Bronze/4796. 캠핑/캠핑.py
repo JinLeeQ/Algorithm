71f1ec4204d7ab9ee6d32e79be3dc14e9b990af4
JinLeeQ <154844595+JinLeeQ@users.noreply.github.com>
2024-04-30 15:46:42 +0100

    [Bronze I] Title: 캠핑, Time: 96 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4796. 캠핑/캠핑.py
@@ -0,0 +1,10 @@
+i = 1
+while True:
+    l, p, v = map(int, input().split())
+    
+    if p==0 and l==0 and v==0: # 0, 0, 0이 입력되면 반복문 종료
+        break
+        
+    camping = (l * (v // p)) + min((v % p), l) # 캠핑장을 사용할 수 있는 최대 일수
+    print('Case {}:'.format(i), camping)
+    i += 1
